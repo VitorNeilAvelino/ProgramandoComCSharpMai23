a2bf80dd9b0b6ab028702cbecc400a6e38c24941
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-06-13 21:48:57 -0300

    Gravação do cliente

--- Fintech.Correntista.Wpf/MainWindow.xaml.cs
+++ Fintech.Correntista.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fintech.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,64 @@ namespace Fintech.Correntista.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() // Método construtor.
         {
             InitializeComponent();
+            PopularControles();
+        }
+
+        private void PopularControles()
+        {
+            sexoComboBox.Items.Add(Sexo.Feminino);
+            sexoComboBox.Items.Add(Sexo.Masculino);
+            sexoComboBox.Items.Add(Sexo.Outro);
+            //sexoComboBox.Items.Add();
+        }
+
+        private void incluirClienteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //object meuObjeto = "Vítor";
+            //meuObjeto = 42;
+            //meuObjeto = false;
+
+            //Cliente cliente = new Cliente();
+            //Cliente cliente = new();
+
+            var cliente = new Cliente();
+
+            cliente.Cpf = cpfTextBox.Text;
+            cliente.DataNascimento = Convert.ToDateTime(dataNascimentoTextBox.Text);
+            cliente.Nome = nomeTextBox.Text;
+            cliente.Sexo = (Sexo)sexoComboBox.SelectedItem;
+
+            Endereco endereco = new();
+            endereco.Logradouro = logradouroTextBox.Text;
+            endereco.Cep = cepTextBox.Text;
+            endereco.Numero = numeroLogradouroTextBox.Text;
+            endereco.Cidade = cidadeTextBox.Text;
+
+            cliente.EnderecoResidencial = endereco;
+
+            //Gravar(cliente);
+
+            clienteDataGrid.Items.Add(cliente);
+            clienteDataGrid.Items.Refresh();
+
+            MessageBox.Show("Cliente cadastrado com sucesso.");
+            LimparControlesCliente();
+            pesquisaClienteTabItem.Focus();
+        }
+
+        private void LimparControlesCliente()
+        {
+            cpfTextBox.Clear();
+            nomeTextBox.Text = "";
+            dataNascimentoTextBox.Text = null;
+            sexoComboBox.SelectedIndex = -1;
+            logradouroTextBox.Text = string.Empty;
+            numeroLogradouroTextBox.Clear();
+            cidadeTextBox.Clear();
+            cepTextBox.Clear();
         }
     }
 }
