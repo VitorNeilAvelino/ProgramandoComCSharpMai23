7d940a7e897080476eff388661b51148ae7b4c03
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-06-14 21:57:59 -0300

    Construtor no gerador de senha

---
+++ CSharp.Capitulo02.GeradorSenha/Senha.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Capitulo02.GeradorSenha
+{
+    public class Senha
+    {
+        public const int TamanhoMinimo = 4;
+
+        public Senha()
+        {
+            Valor = Gerar();
+        }
+
+        public int Tamanho { get; set; } = TamanhoMinimo;
+        public string Valor { get; set; }
+
+        public string Gerar()
+        {
+            var senha = "";
+            var randomico = new Random();
+
+            for (int i = 1; i <= Tamanho; i++)
+            {
+                var algarismo = randomico.Next(10);
+
+                senha += algarismo;
+            }
+
+            return senha;
+        }
+    }
+}
---
+++ CSharp.Capitulo02.GeradorSenhaTests/SenhaTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp.Capitulo02.GeradorSenha;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Capitulo02.GeradorSenha.Tests
+{
+    [TestClass()]
+    public class SenhaTests
+    {
+        [TestMethod()]
+        public void GerarTest()
+        {
+            //Senha.Gerar();
+
+            var senha = new Senha();
+            
+            senha.Tamanho = 4;
+
+            var valor = senha.Gerar();
+
+            Assert.AreEqual(senha.Tamanho, valor.Length);
+
+            Console.WriteLine(valor);
+        }
+
+        [TestMethod]
+        public void ConstrutorSemParametroDeveRetornarSenhaMinima()
+        {
+            var senha = new Senha();
+
+            Assert.AreEqual(senha.Valor.Length, Senha.TamanhoMinimo);
+
+            Console.WriteLine(senha.Valor);
+        }
+    }
+}
