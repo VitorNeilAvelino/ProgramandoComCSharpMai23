673cd396b025a28f143b0e7acf742ca20e47815d
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-06-21 21:54:19 -0300

    MovimentoRepositorio

---
+++ Fintech.Repositorios.SistemaArquivos/MovimentoRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Fintech.Dominio.Entidades;
+
+namespace Fintech.Repositorios.SistemaArquivos
+{
+    public class MovimentoRepositorio
+    {
+        private const string DiretorioBase = "Dados";
+
+        public void Inserir(Movimento movimento)
+        {
+            var registro = $"{movimento.Guid}|{movimento.Conta.Agencia.Numero}|{movimento.Conta.Numero}|" +
+                $"{movimento.Data}|{(int)movimento.Operacao}|{movimento.Valor}";
+
+            if (!Directory.Exists(DiretorioBase))
+            {
+                Directory.CreateDirectory(DiretorioBase);
+            }
+
+            File.AppendAllText(@$"{DiretorioBase}\Movimento.txt", registro + Environment.NewLine);
+        }
+    }
+}
