322a75f352d72533ecfd5f53de63d4bfb7c48e68
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-07-03 21:26:46 -0300

    De delegate à lâmbda expression Teste com calculadora

---
+++ CSharp.Capitulo10.DelegateLambda.Testes/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Capitulo10.DelegateLambda.Testes
+{
+    public delegate int EfetuarOperacao(int valor1, int valor2);
+
+    public static class Calculadora
+    {
+        private static int Somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static EfetuarOperacao ObterOperacao(TipoOperacao tipoOperacao)
+        {
+            switch (tipoOperacao)
+            {
+                case TipoOperacao.Soma:
+                    return Somar;
+                case TipoOperacao.Subtracao:
+                    return Subtrair;
+                case TipoOperacao.Multiplicacao:
+                    break;
+                case TipoOperacao.Divisao:
+                    break;
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
